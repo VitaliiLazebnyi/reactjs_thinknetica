8885f963be37e26d557c096a7d835adac1353501
Vitalii Lazebnyi <18521734+VitaliiLazebnyi@users.noreply.github.com>
2021-04-24 17:56:23 +0300

    Task 2.1. Header&Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from 'react'
 import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer"
 import Title from "./components/Title";
 
 function App(props) {
   return (
-    <div className="App">
-      <Title title={props.title} />
-    </div>
+      <React.Fragment>
+          <Header />
+          <Title title={props.title} />
+          <Footer />
+      </React.Fragment>
   );
 }
 
