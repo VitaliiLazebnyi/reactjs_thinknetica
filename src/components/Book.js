7ba3122d78b927c1a44effb43f5a0ae3502f861d
Vitalii Lazebnyi <18521734+VitaliiLazebnyi@users.noreply.github.com>
2021-04-28 17:53:50 +0300

    book styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,11 +4,26 @@ class Book extends Component {
     render(){
         return (
             <div className='book'>
-                Title: {this.props.book.title}
-                Description: {this.props.book.description}
-                Pages: {this.props.book.pages}
-                Progress: {this.props.book.progress}
-                Image: {this.props.book.image}
+                <div className='row'>
+                    <span className='column'>Title</span>:
+                    <span className='title'>{this.props.book.title}</span>
+                </div>
+                <div className='row'>
+                    <span className='column'>Description</span>:
+                    <span className='description'>{this.props.book.description}</span>
+                </div>
+                <div className='row'>
+                    <span className='column'>Pages</span>:
+                    <span className='pages'>{this.props.book.pages}</span>
+                </div>
+                <div className='row'>
+                    <span className='column'>Progress</span>:
+                    <span className='progress'>{this.props.book.progress}</span>
+                </div>
+                <div className='row'>
+                    <span className='column'>Image</span>:
+                    <span className='image'>{this.props.book.image}</span>
+                </div>
             </div>
         )
     }
