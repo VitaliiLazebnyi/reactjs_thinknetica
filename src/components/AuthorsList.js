71f8295160cdd69c4305864180810fd1e6b77e8c
Vitalii Lazebnyi <18521734+VitaliiLazebnyi@users.noreply.github.com>
2021-05-04 13:31:47 +0300

    switch number of displayed authors

--- src/components/AuthorsList.js
+++ src/components/AuthorsList.js
@@ -1,12 +1,44 @@
-import Author from "./Author";
+import React, { Component } from 'react'
+import Author from './Author'
 
-function AuthorsList(props) {
-    const authorsList = props.authors.map(
-        (author) => <Author key={author.email} author={author}/>
-    );
-    return (
-        <div className="authors">{authorsList}</div>
-    );
+class AuthorsList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            full: false
+        };
+
+        this.authors = this.props.authors;
+        this.switchView = this.switchView.bind(this)
+    }
+
+    switchView() {
+        this.setState({ full: !this.state.full })
+    }
+
+    showMoreItem() {
+        const text = this.state.full ? 'Show less' : 'Show more'
+        return <div className='showMore' onClick={this.switchView}>{text}</div>
+    }
+
+    authorsList() {
+        const size = this.state.full ? this.authors.length : 3
+        const authorsList = this.authors.slice(0, size).map(
+            (author) => <Author key={author.email} author={author} />
+        )
+        return authorsList;
+    }
+
+    render(){
+        return (
+            <div>
+                <div className="authors">
+                    {this.authorsList()}
+                    {this.showMoreItem()}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default AuthorsList
